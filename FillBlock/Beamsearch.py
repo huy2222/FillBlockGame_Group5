e63164fd8d24bc62a4f86eee594b096304e3b065
hgdinh2k5 <Hhgadh123@gmail.com>
2025-10-15 23:23:27 +0700

    Update Beamsearch.py

--- FillBlock/Beamsearch.py
+++ FillBlock/Beamsearch.py
@@ -22,10 +22,7 @@ def calculate_cost(path, white_cells, n):
         y = np[1] + dy[i]
         if (x, y) in white_cells and (x, y) not in path_set:
             cost += 1
-    if cost == 0 and (len(path) < len(white_cells)):
-        return 1000
-    else:
-        return cost
+    return cost
 
 
 
@@ -42,24 +39,7 @@ def check(path, white_cells, n):
 
     return cost
 
-    
-
-
-def generate_next_state(white_cells, N, path):
-    np = path[-1]
-    next_paths = []
-    dx = [-1, 0, 1, 0]
-    dy = [0, -1, 0, 1]
-    for i in range(4):
-        x = np[0] + dx[i]
-        y = np[1] + dy[i]
-        if is_valid(white_cells, N, path, (x, y)):
-            new_path = path + [(x, y)]
-            cost = calculate_cost(new_path, white_cells, N)
-            next_paths.append((cost, new_path))
-    return next_paths
-
-def beamsearch(start_pos, white_cells, N, beam_width = 20):
+def beamsearch(start_pos, white_cells, N, beam_width = 500):
     frontier = [[start_pos]]
     step = 0
     while frontier:
@@ -76,7 +56,7 @@ def beamsearch(start_pos, white_cells, N, beam_width = 20):
             break
         
         
-        new_frontier.sort(key=lambda x: (x[0], check(x[1], white_cells, N)))
+        new_frontier.sort(key=lambda x: (x[0]))
         k = beam_width
         if k > len(new_frontier):
             k = len(new_frontier)
